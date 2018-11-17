54b15c51f9d6a2830838519a460ef16658a619f3
kat-onyx <sarah.kathryn.peters@gmail.com>
2018-11-16 16:49:55 -0800

    Create specs for signing up a user:

--- goalapp/spec/features/auth_spec.rb
+++ goalapp/spec/features/auth_spec.rb
@@ -5,13 +5,20 @@ feature 'the signup process' do
   background :each do 
     visit(new_user_path)
   end
+  
   scenario 'has a new user page' do
     expect(page).to have_content('sign up')
   end
     
   feature 'signing up a user' do
-
-    scenario 'shows username on the homepage after signup'
+    scenario 'shows username on the homepage after signup' do 
+      fill_in 'username', with: 'kat'
+      fill_in 'password', with: 'password'
+      click_button 'sign up'
+      expect(page).to have_content('kat')
+      user = User.find_by(username: 'kat')
+      expect(current_path).to eq(user_path(user))
+    end 
 
   end
 end
